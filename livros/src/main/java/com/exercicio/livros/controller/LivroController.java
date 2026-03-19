f4d208805af8589873c698e07e5fa0bb54dbb53d
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-19 11:20:25 -0300

    adicionado controller REST e endpoints

---
+++ livros/src/main/java/com/exercicio/livros/controller/LivroController.java
@@ -0,0 +1,43 @@
+package com.exercicio.livros.controller;
+
+import com.exercicio.livros.entity.Livro;
+import com.exercicio.livros.service.LivroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class LivroController {
+    private final LivroService livroService;
+
+    public LivroController(LivroService livroService) {
+        this.livroService = livroService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Livro> criar (@RequestBody Livro livro) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroService.salvar(livro));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Livro>> listarTodos() {
+        return ResponseEntity.ok(livroService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Livro> buscarPorId(@PathVariable Long id) {
+        return livroService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        livroService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
