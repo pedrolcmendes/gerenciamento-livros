59325b903a7b70765d2d986f6eac78bc1cf63305
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-19 11:09:49 -0300

    adicionado o service LivroService

---
+++ livros/src/main/java/com/exercicio/livros/service/LivroService.java
@@ -0,0 +1,35 @@
+package com.exercicio.livros.service;
+
+import com.exercicio.livros.entity.Livro;
+import com.exercicio.livros.repository.LivroRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LivroService {
+
+    private final LivroRepository livroRepository;
+
+    public LivroService(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    public Livro salvar(Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    public List<Livro> listarTodos() {
+        return livroRepository.findAll();
+    }
+
+    public Optional<Livro> buscarPorId(Long id) {
+        return livroRepository.findById(id);
+    }
+
+    public void deletar(long id) {
+        livroRepository.deleteById(id);
+    }
+
+}
